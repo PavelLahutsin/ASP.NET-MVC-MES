ec432aeeff2340f7b2595950f8b5e6541945d53b
Pavel <p.lahutsin@gmail.com>
2018-03-13 19:59:40 +0300

    )

--- MES.WEB/Controllers/AssemblyController.cs
+++ MES.WEB/Controllers/AssemblyController.cs
@@ -79,35 +79,15 @@ namespace MES.WEB.Controllers
             return Json(result, JsonRequestBehavior.AllowGet);
 
         }
+
+        public  ActionResult Music()
+        {            
+            return PartialView();
+        }
+        public ActionResult Book()
+        {
+            return PartialView();
+        }
     }
-
-    //public class ValidateAjaxAttribute : ActionFilterAttribute
-    //{
-    //    public override void OnActionExecuting(ActionExecutingContext filterContext)
-    //    {
-    //        if (!filterContext.HttpContext.Request.IsAjaxRequest())
-    //            return;
-
-    //        var modelState = filterContext.Controller.ViewData.ModelState;
-    //        if (!modelState.IsValid)
-    //        {
-    //            var errorModel =
-    //                from x in modelState.Keys
-    //                where modelState[x].Errors.Count > 0
-    //                select new
-    //                {
-    //                    key = x,
-    //                    errors = modelState[x].Errors.
-    //                        Select(y => y.ErrorMessage).
-    //                        ToArray()
-    //                };
-    //            filterContext.Result = new JsonResult()
-    //            {
-    //                Data = errorModel
-    //            };
-    //            filterContext.HttpContext.Response.StatusCode =
-    //                (int)HttpStatusCode.BadRequest;
-    //        }
-    //    }
-    //}
+    
 }
